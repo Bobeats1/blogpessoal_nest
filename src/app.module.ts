094b9619b37dfbf8c7ac05abd06597304c93a9db
Bobeats . <gabriel.gomes86@gmail.com>
2025-12-09 13:10:56 -0300

    Adicionados os métodos de autenticação

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Postagem } from './postagem/entities/postagem.module.entity';
+import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
-import { Tema } from './tema/entities/tema.entity';
-import { TemaModule } from './tema/tema.module';
+import { TemaModule } from './temas/tema.module';
+import { Tema } from './temas/entities/tema.entity';
+import { AuthModule } from './auth/auth.module';
+import { Usuario } from './usuario/entities/usuario.entity';
+import { UsuarioModule } from './usuario/usuario.module';
+
+// Decorator - Etiqueta de Metadados
 @Module({
-  imports: [
+  imports: [  // Configurando o TypeORM
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
@@ -13,13 +18,15 @@ import { TemaModule } from './tema/tema.module';
       username: 'root',
       password: 'root',
       database: 'db_blogpessoal',
-      entities: [Postagem, Tema],
+      entities: [Postagem, Tema, Usuario],
       synchronize: true,
     }),
     PostagemModule,
-    TemaModule
+    TemaModule,
+    AuthModule,
+    UsuarioModule
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-// Ajustando a Timezone do Banco de Dados 
-  process.env.TZ = '03:00'
 
+  // Ajustando o Fuso Horário do BD
+  process.env.TZ = '-03:00';
+
+  // Aplicando os recursos de validação
   app.useGlobalPipes(new ValidationPipe());
 
+  // Habilitando o CORS do projeto
   app.enableCors();
 
-  await app.listen(process.env.PORT ?? 4000);
+  // Indico qual porta o projeto está sendo executado
+  await app.listen(4000);
 }
+
 bootstrap();
+
